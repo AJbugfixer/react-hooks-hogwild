8402688c0cc662ff6fb379a892c5e4710ee3a6d5
Ajay <ajay.ip3105@gmail.com>
2022-06-06 00:00:55 -0400

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import Nav from "./Nav";
+import Hogs from "./Hogs.JS";
 
-import hogs from "../porkers_data";
+import FilterHogs from "./FilterHogs";
+
 
 function App() {
+	const [hogsArr, setHogs] = useState(Hogs)
+	
 	return (
 		<div className="App">
 			<Nav />
+			<FilterHogs hogs={hogsArr} setHogs={setHogs} />
+			< Hogs hogs={hogsArr} />
 		</div>
 	);
 }
